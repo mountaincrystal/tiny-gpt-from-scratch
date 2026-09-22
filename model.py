245bff4769bbb167414d292f79e91b63cb568c3d
mountaincrystal <claymorecrystal@outlook.com>
2026-09-22 15:06:47 +0900

    Step 166: decode_final_sequence

--- model.py
+++ model.py
@@ -1614,6 +1614,9 @@ def generation_loop_for_n_steps(params, prompt_ids, n_new_tokens, block_size, te
         context = append_token_to_sequence(context, token)
     return context
 
-# Step 166 - decode_final_sequence (not yet solved)
-# TODO: implement
+# Step 166 - decode_final_sequence
+def decode_final_sequence(generated_ids, itos):
+    """Decode a (1, T) id tensor into a string using itos."""
+    # TODO: take the single row of generated_ids and decode it into a string.
+    return decode_ids(generated_ids[0].tolist(), itos)
 
