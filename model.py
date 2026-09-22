16eb7f78f1d938e536ce5e7d6872e1e8a1960054
mountaincrystal <claymorecrystal@outlook.com>
2026-09-22 13:56:55 +0900

    Step 128: merge_heads_to_d_model

--- model.py
+++ model.py
@@ -1098,8 +1098,13 @@ def get_multihead_output_sequence_length(x_heads_back):
     # TODO: read the sequence-length dimension from x_heads_back's shape
     return int(x_heads_back.shape[1])
 
-# Step 128 - merge_heads_to_d_model (not yet solved)
-# TODO: implement
+# Step 128 - merge_heads_to_d_model
+import numpy as np
+
+def merge_heads_to_d_model(x_heads_back):
+    """Reshape (B, T, n_heads, d_head) into (B, T, d_model)."""
+    # TODO: collapse the last two axes into a single d_model axis
+    return x_heads_back.reshape(x_heads_back.shape[0], x_heads_back.shape[1], -1)
 
 # Step 129 - multihead_output_projection_forward (not yet solved)
 # TODO: implement
