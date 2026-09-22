4d2c5e621943774a50ebe43c257591c7bb346a5b
mountaincrystal <claymorecrystal@outlook.com>
2026-09-22 13:48:08 +0900

    Step 122: get_multihead_sequence_length

--- model.py
+++ model.py
@@ -1047,8 +1047,13 @@ def get_multihead_n_heads(config):
     # TODO: return the number of attention heads stored in the multi-head config dict.
     return config['n_heads']
 
-# Step 122 - get_multihead_sequence_length (not yet solved)
-# TODO: implement
+# Step 122 - get_multihead_sequence_length
+import numpy as np
+
+def get_multihead_sequence_length(x):
+    """Return T from x of shape (B, T, d_model)."""
+    # TODO: return the sequence length T from the (B, T, d_model) tensor.
+    return get_array_shape(x)[1]
 
 # Step 123 - compute_d_head (not yet solved)
 # TODO: implement
